72ed17250c518cfe23bf26713cfecf454486f2a2
cgb14 <844386320@qq.com>
2016-10-12 23:43:29 +0800

    新增更新设备信息接口和查找用户接口，并且将返回数据改为json格式

--- firekylin_php/controllers/FirekylinController.php
+++ firekylin_php/controllers/FirekylinController.php
@@ -11,6 +11,7 @@ namespace app\controllers;
 use yii\web\Controller;
 use app\models\statistic;
 use app\models\user;
+use app\models\userdevice;
 use Yii;
 
 
@@ -28,34 +29,46 @@ class FirekylinController extends Controller
         $user->id = $post_data['id'];
         $user->name = $post_data['name'];
         $user->password = $post_data['password'];
-        $user->save();
-        echo('success');
+        if($user->save())
+            return json_encode(['message'=>'success']);
+        return json_encode(['message'=>'fail']);
+
     }
 
     public function actionFinduser()
     {
         $post_data = Yii::$app->request->post();
         $userName = $post_data['name'];
-
-
         if(User::findUser($userName))
         {
-            return 'success';
+            $user_id = User::findOne(['name'=>$userName])->id;
+            return json_encode(['message'=>'success','user_id'=>$user_id]);
         }
         else
         {
-            return 'fail';
+            return json_encode(['message'=>'fail','user_id'=>'0']);
         }
     }
 
-//    public function actionUserdevice()
-//    {
-//        $post_data = Yii::$app->request->post();
-//
-//    }
+    public function actionUserdevice()
+    {
+        $post_data = Yii::$app->request->post();
+        $user_id = $post_data['user_id'];
+        $os_type = $post_data['os_type'];
+        $channel = $post_data['channel'];
+        $device_id = $post_data['device_id'];
+        $user_device = UserDevice::findOne(['user_id'=>$user_id]);
+        if($user_device == null)
+            $user_device = new UserDevice();
+        $user_device->user_id = $user_id;
+        $user_device->os_type = $os_type;
+        $user_device->channel = $channel;
+        $user_device->device_id = $device_id;
+        $user_device->save();
+    }
 
     public function actionIndex()
     {
-        echo(json_encode(['name'=>'wangdong','password'=>'wangdong']));
+        echo(json_encode(['user_id'=>'666','os_type'=>'apple','channel'=>'light','device_id'=>'2132']));
     }
 }
